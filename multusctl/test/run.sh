2fb962761672e1781e6fb3ec8ac99ee0d3b5a631
Jose Nuñez <jonunyez@gmail.com>
2021-08-24 13:10:17 +0200

    clean resources

--- multusctl/test/run.sh
+++ multusctl/test/run.sh
@@ -1,21 +1,29 @@
 #!/bin/bash
+set -e 
 
-function test () {
-
+t() {
+  local PARENT_IFACE="$1"
   #install sdn
-  kubectl create -f ./assets/kube-flannel.yml
-  #install multus
-  ./multusctl install --wait
-  #create network attachment called 'netat' 
-  ./multusctl create netat --url=./assets/config-macvlan.yml
+  kubectl apply -f ./assets/kube-flannel.yml
 
   #create macvlan bridge
-  sudo ip link add data-plane-vlan link $MASTER_IFACE type macvlan mode bridge
+  sudo ip link delete data-plane-vlan
+  sudo ip link add data-plane-vlan link $PARENT_IFACE type macvlan mode bridge
   sudo ip link set data-plane-vlan up
 
+  #install multus
+  ../multusctl uninstall
+  ../multusctl install --wait
+  #create network attachment called 'netat' 
+  ../multusctl create netat --url=assets/config-macvlan.yaml
+
   #create pod using data-plane-vlan using the new network attachment
-  kubectl create -f ./assets/pod.yml
+  kubectl apply -f ./assets/pod.yml
+  
+  #veryfi pod has multiple interfaces
+  #kubectl exec -it samplepod -- ip a s
 }
 
-#MASTER_IFACE is the NIC name of the parent interface that will be added to k8s using multus
-test $MASTER_IFACE
+#HOST_IFACE is the NIC name of the parent interface that will be added to k8s using multus
+
+t "enx24f5a2f13bd3"
